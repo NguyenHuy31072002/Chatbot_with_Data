3e497889d06b36b92cd735e37cad889cf2ccc6e6
khachuy7 <huyk93345@gmail.com>
2024-08-20 12:35:53 +0700

    chatbot-with-data

---
+++ src/models/llms.py
@@ -0,0 +1,54 @@
+from langchain_community.llms import CTransformers
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from transformers import AutoModelForCausalLM, AutoTokenizer
+# Cau hinh
+model_file = "/home/huy31072002/Desktop/Chatbot/chatbot/models/vinallama-7b-chat_q5_0.gguf"
+
+# Load LLM
+def load_llm(model_file):
+    """load models
+
+    Args:
+        model_file (_type_): _description_
+
+    Returns:
+        _type_: model
+    """
+    llm = CTransformers(
+        model=model_file,
+        model_type="llama",
+        max_new_tokens=1024,
+        temperature=0.01
+    )
+    return llm
+ 
+
+# Tao prompt template
+def creat_prompt(template):
+    prompt = PromptTemplate(template = template, input_variables=["question"])
+    return prompt
+
+
+# Tao simple chain
+def create_simple_chain(prompt, llm):
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    return llm_chain
+
+# Chay thu chain
+template = """<|im_start|>system
+Bạn là một trợ lí AI hữu ích. Hãy trả lời người dùng một cách chính xác.
+<|im_end|>
+<|im_start|>user
+{question}<|im_end|>
+<|im_start|>assistant"""
+
+prompt = creat_prompt(template)
+# llm = load_llm(model_file)
+llm = load_llm(model_file)
+
+llm_chain = create_simple_chain(prompt, llm)
+
+question = "Bạn có biết chủ tịch Hồ Chí Minh không ?"
+response = llm_chain.invoke({"question":question})
+print(response)
